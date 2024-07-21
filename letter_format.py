8f0f18c229e965e3802b429740c41595d7c090df
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-07-21 18:50:44 +0530

    Add files via upload

---
+++ letter_format.py
@@ -0,0 +1,12 @@
+letter = '''Dear <|NAME|>
+Greeting from ABC coding hourse
+You are Selected
+
+Date. <|DATE|>'''
+
+name=input("Enter your name :")
+date=input("Enter today Date:")
+letter=letter.replace("<|NAME|>", name)
+letter=letter.replace("<|DATE|>", date)
+
+print(letter)
