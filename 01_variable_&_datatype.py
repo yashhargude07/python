27d09af65a3df753f3f1b95477599b8f3a5a1762
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-07-21 09:31:13 +0530

    Add files via upload

---
+++ 01_variable_&_datatype.py
@@ -0,0 +1,24 @@
+a = "yash"
+d ='''santosh'''
+b = 354
+c = 3.24
+f=True
+g=False
+
+# printing the variable
+print(a)
+print(d)
+print(b)
+print(c)
+print(f)
+print(g)
+
+
+# printing the type of varisble
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(type(f))
+print(type(g))
