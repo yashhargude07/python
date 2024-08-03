aa4b8364feee6fbdf82562dba3e504cc9875f7ab
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-03 08:27:34 +0530

    Add files via upload

---
+++ string_function_find_the_length_of_char.py
@@ -0,0 +1,7 @@
+story= '''Visual Studio Code is a free coding editor that helps you start coding quickly. 
+Use it to code in any programming language, without switching editors. Visual 
+Studio Code has support for many languages, including Python, Java, C++, JavaScript, and more.'''
+
+# string function
+
+print(len(story)) 
