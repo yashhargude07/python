5dc7f2a8ddb9d9580879a681157c823ba2af1e3f
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-03 08:52:11 +0530

    Add files via upload

---
+++ student_marks_input_value_in_list.py
@@ -0,0 +1,12 @@
+m1 = int(input("Enter student marks number 1:"))
+m2 = int(input("Enter student marks number 2:"))
+m3 = int(input("Enter student marks number 3:"))
+m4 = int(input("Enter student marks number 4:"))
+m5 = int(input("Enter student marks number 5:"))
+m6 = int(input("Enter student marks number 6:"))
+
+studentmakrs = [m1, m2, m3, m4, m5, m6 ]
+
+studentmakrs.sort()
+
+print(studentmakrs)
