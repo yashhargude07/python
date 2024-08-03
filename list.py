8dd08f012711d078cef2fd5db9230a39c9d58569
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-03 08:40:08 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+# create the list using []
+a = [1,34,56,78,90,24,5]
+
+# Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2] etc..
+print(a[2])
+
+# change the value of list using 
+a[0]=50
+print(a)
+
+# we can create a list with items of different type
+
+b=[10,"yash",False,9.5]
+print(b)
+
+# list sclicing
+
+frend=["yash","dhanu","tejas","vishal","mayur","pranit"]
+print(frend[0:4])
