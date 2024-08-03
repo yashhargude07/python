14eae6d00dff58dd6879403c899a1f6a12b12604
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-03 08:58:36 +0530

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,30 @@
+list_1 = [1,8,7,2,21,15]
+
+# sorting the list
+list_1.sort()
+print(list_1)
+
+ # reverse:- reverse sorting the list
+list_2 = [1,8,7,2,21,15]
+list_2.reverse()
+print(list_2)
+
+#append :-adds at end of the list
+list_3 = [1,8,7,2,21,15]
+list_3.append(45)
+print(list_3)
+
+#insert:- insert the varable in string
+l4 = [1,8,7,2,21,15]
+l4.insert(1,544)
+print(l4)
+
+#pop:- will delete the element at index x,y,z, and return it's value
+list_5 = [1,8,7,2,21,15]
+list_5.pop(2)
+print(list_5)
+
+#remove:- remove the list of element
+list_6 = [1,8,7,2,21,15]
+list_6.remove(21)
+print(list_6)
