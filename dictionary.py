07e91dbf3baf522020fab52d807371558520250b
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-15 17:33:45 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+myDict = {
+    "fast": "in a Quick manner",
+    "yash": "A coder",
+    "mark": [2,3,4,6,8]
+}
+print(myDict["fast"])
+
+#change the dictionary items.
+myDict["mark"]=[45,56,67,98]
+
+print(myDict["yash"])
+print(myDict["mark"])
