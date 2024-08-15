bbe98b7252bbe95f1da50aaffb0f2ec823369d70
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-15 18:09:26 +0530

    Add files via upload

---
+++ display_unique_number.py
@@ -0,0 +1,14 @@
+# the programm is to input eight number from the user and display all the unique number..
+
+
+num1 = int(input("Enter number 1:-"))
+num2 = int(input("Enter number 2:-"))
+num3 = int(input("Enter number 3:-"))
+num4 = int(input("Enter number 4:-"))
+num5 = int(input("Enter number 5:-"))
+num6 = int(input("Enter number 6:-"))
+num7 = int(input("Enter number 7:-"))
+num8 = int(input("Enter number 8:-"))
+
+a={num1, num2, num3, num4, num5, num6, num7, num8}
+print(a)
