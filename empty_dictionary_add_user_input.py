8d025b42775f001141d222e621612cb80bf738c5
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-15 18:20:34 +0530

    Add files via upload

---
+++ empty_dictionary_add_user_input.py
@@ -0,0 +1,13 @@
+favlang = {}
+a = input("Enter your favorite language yash:-")
+b = input("Enter your favorite language dhanu:-")
+c = input("Enter your favorite language tejas:-")
+d = input("Enter your favorite language vishal:-")
+e = input("Enter your favorite language yash:-")
+favlang["yash"] = a
+favlang["dhanu"] = b
+favlang["tejas"] = c
+favlang["vishal"] = d
+favlang["yash"] = e
+
+print(favlang)
