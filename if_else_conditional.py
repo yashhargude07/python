6ed3a79fc1c6b0a9636ae076e628956a69f4e8fa
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-08-18 09:23:22 +0530

    Add files via upload

---
+++ if_else_conditional.py
@@ -0,0 +1,11 @@
+
+a=10
+
+if(a<5):
+    print("The value of a less than 5")
+elif(a>5):
+    print("The value of a greater than 5")
+elif(a<5):
+    print("The value of a less than 5")
+else:
+    print("The value of a greater than 10")
