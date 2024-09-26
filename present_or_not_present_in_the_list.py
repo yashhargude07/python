bf23826177e74c51a0755c39d3c5fedb336a5af6
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-09-26 22:27:00 +0530

    Add files via upload

---
+++ present_or_not_present_in_the_list.py
@@ -0,0 +1,7 @@
+names=["yash","tejas","vishal","mayur","dhanu","pranit"]
+name = input("Enter the name to check\n")
+
+if name in names:
+    print("Your name is present in list")
+else:
+    print("Your name is not present in the list")
